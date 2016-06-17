3048a96204062ed475ddc8eedccf0580359a61cf
thomasjacobjr <thomasjacobjr@gmail.com>
2016-06-17 11:55:19 -0700

    Completed Basic Nav Bar, routes, and views to static pages

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,4 +4,16 @@ class WelcomeController < ApplicationController
 
   def about
   end
+
+  def upcoming_events
+  end
+
+  def contacts
+  end
+
+  def photo_gallery
+  end
+
+  def video_gallery
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ Rails.application.routes.draw do
 
   get 'welcome/about'
 
+  get 'welcome/upcoming_events'
+
+  get 'welcome/contacts'
+
+  get 'welcome/photo_gallery'
+
+  get 'welcome/video_gallery'
+
   root 'welcome#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
