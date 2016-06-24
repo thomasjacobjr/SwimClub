2ba06a62c8966a064bf7a47936a64ab70f0b7f38
thomasjacobjr <thomasjacobjr@gmail.com>
2016-06-23 18:38:05 -0700

    Seeded temp Pages data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# require 'random_data'
+
+N_PAGES = 50
+
+# Create Pages
+N_PAGES.times do
+  Page.create!(
+    title: Faker::Hipster.sentence,
+    body: Faker::Hipster.paragraph
+  )
+end
+
+puts "Seed finished"
+puts "#{N_PAGES} additional pages created."
