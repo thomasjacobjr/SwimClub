5709e16c257e849173b50cffb5f66361b969cc68
thomasjacobjr <thomasjacobjr@gmail.com>
2016-06-23 18:54:06 -0700

    Pages Controller actions defined

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,24 +1,44 @@
 class PagesController < ApplicationController
 
   def index
+    @pages = Page.all 
   end
 
   def show
+    @page = Page.find(params[:id])
   end
 
   def new
+    @page = Page.new
   end
 
   def create
+    @page = Page.new
+    @page.title = params[:page][:title]
+    @page.body = params[:page][:body]
   end
 
   def edit
+    @page = Page.find(params[:id])
   end
 
   def update
+    @page = Page.find(params[:id])
+
+    @page.title = params[:page][:title]
+    @page.body = params[:page][:body]
   end
 
   def destroy
+    @page = Page.find(params[:id])
+
+    if @page.destroy
+      flash[:notice] = "\"#{@page.title}\" was deleted successfully."
+      redirect_to pages_path
+    else
+      flash.now[:alert] = "There was an error deleting the page."
+      render :show
+    end
   end
 
 end
