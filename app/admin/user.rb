e85c94d4f46b1aa0a05369709b398efb429e81fb
thomasjacobjr <thomasjacobjr@gmail.com>
2016-06-25 15:32:42 -0700

    Admin can create new user with email and admin status

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,5 +1,13 @@
 ActiveAdmin.register User do
-  filter :email 
+  filter :email
+
+  form do |f|
+    inputs 'Details' do
+      input :email
+      input :admin
+      actions 
+    end
+  end
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
