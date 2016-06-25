4347884c39da89a870786ec302249669b1b1e6cc
thomasjacobjr <thomasjacobjr@gmail.com>
2016-06-25 15:07:59 -0700

    Simpler attribute table ActiveAdmin User Resource

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -4,7 +4,12 @@ ActiveAdmin.register User do
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
 # permit_params :list, :of, :attributes, :on, :model
-#
+  index do
+    selectable_column
+    column :email
+    column :admin
+    column :created_at
+  end
 # or
 #
 # permit_params do
