31ef3a2041721f73ee4d508d77c68dbb63c8bfb8
thomasjacobjr <thomasjacobjr@gmail.com>
2016-06-25 12:28:00 -0700

    Various attempts to implement ActiveAdmin (in Progress)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def authenticate_admin_user!
+    redirect_to new_user_session_path unless current_user.try(:is_admin?)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable 
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  def is_admin?
+    self.email && ENV['ADMIN_EMAILS'].to_s.include?(self.email)
+  end
 end
