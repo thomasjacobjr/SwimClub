36a0a1e6836c9cc1bc1cb4f8842e6b321ca20ffd
thomasjacobjr <thomasjacobjr@gmail.com>
2016-07-04 13:44:28 -0700

    Turned off confirm email for admin user creation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :letter_opener
+  #config.action_mailer.delivery_method = :letter_opener
 
 
   # Don't care if the mailer can't send.
