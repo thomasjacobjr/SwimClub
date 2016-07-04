fc3e2535bea4cedfaa46abcf851e168a36542046
thomasjacobjr <thomasjacobjr@gmail.com>
2016-07-04 12:14:46 -0700

    Configured page.rb ActiveAdmin with editable params and friendly_id support

--- app/admin/page.rb
+++ app/admin/page.rb
@@ -1,4 +1,28 @@
 ActiveAdmin.register Page do
+  controller do
+    def find_resource
+      scoped_collection.friendly.find(params[:id])
+    end
+  end
+
+  filter :menu_title
+  permit_params [:menu_title, :title, :body]
+
+  form do |f|
+    f.inputs 'Details' do
+      f.input :menu_title
+      f.input :title
+      f.input :body
+    end
+    f.actions
+  end
+
+  index do
+    selectable_column
+    column :menu_title
+    column :title
+    actions
+  end
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
