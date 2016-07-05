c8e41ef398fed336738ee82ab1df1fbe04fab5ed
thomasjacobjr <thomasjacobjr@gmail.com>
2016-07-04 19:07:51 -0700

    CKeditor added to activeadmin page creation

--- app/admin/page.rb
+++ app/admin/page.rb
@@ -12,7 +12,7 @@ ActiveAdmin.register Page do
     f.inputs 'Details' do
       f.input :menu_title
       f.input :title
-      f.input :body
+      f.input :body, :as => :ckeditor
     end
     f.actions
   end
