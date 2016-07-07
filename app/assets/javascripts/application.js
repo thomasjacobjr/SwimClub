c95674b8fa8f773bb2869e27e994abf63615cb2b
thomasjacobjr <thomasjacobjr@gmail.com>
2016-07-07 10:55:33 -0700

    Manual install of CKEditor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,14 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
+//= require ckeditor/ckeditor
+
+$(document).ready(function(){
+  CKEDITOR.replace('page_body');
+})
